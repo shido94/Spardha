008b8dbe61b4b621021d21d7c3a36c9561196149
shido94 <rupeshyadav94.ry@gmail.com>
2018-09-24 20:38:55 +0530

    closed totally.

--- app.js
+++ app.js
@@ -64,6 +64,7 @@ app.get('/submit', (req,res) => {
 // });
 
 
+
 const server = http.createServer(app);
 mongoose.connect('mongodb://localhost:27017/fest',{ useNewUrlParser: true });
 const db = mongoose.connection;
